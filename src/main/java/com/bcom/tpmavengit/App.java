cb4d8aac66aaf0c5e9ddb348c3cc681bb1876d4b
Olivier Barais <barais@irisa.fr>
2014-04-24 12:00:30 +0200

    MyFirstCommit

---
+++ src/main/java/com/bcom/tpmavengit/App.java
@@ -0,0 +1,13 @@
+package com.bcom.tpmavengit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
